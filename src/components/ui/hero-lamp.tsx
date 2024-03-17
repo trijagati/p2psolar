9d387d4896f70ca3df821a31a07dbd7c088d7c18
Ashish Chaudhary <ashish080303@gmail.com>
2024-03-17 20:57:52 +0530

    lamp-page done with content and style changes

--- src/components/ui/hero-lamp.tsx
+++ src/components/ui/hero-lamp.tsx
@@ -2,9 +2,11 @@
 import { cn } from "@/utils/cn";
 import { motion } from "framer-motion";
 import React from "react";
+import styles from './styles.module.css'
 
 export function HeroLamp() {
   return (
+
     <LampContainer>
       <motion.h1
         initial={{ opacity: 0.5, y: 100 }}
@@ -14,9 +16,14 @@ export function HeroLamp() {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-      >
-        Build lamps <br /> the right way
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-6xl"
+        >
+        Distribute Energy   <br /> 
+          the 
+        <span className=" text-yellow-200 tracking-normal">
+           {" "}RIGHT{" "}
+          </span> 
+           way
       </motion.h1>
     </LampContainer>
   );
@@ -32,7 +39,7 @@ export const LampContainer = ({
   return (
     <div
       className={cn(
-        "relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-slate-950 w-full rounded-md z-0",
+        "relative pt-60 flex min-h-screen flex-col items-center justify-center overflow-hidden bg-slate-950 w-full rounded-md z-0",
         className
       )}
     >
@@ -96,21 +103,10 @@ export const LampContainer = ({
         <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-950 "></div>
       </div>
 
-      <div className="relative z-50 flex -translate-y-80 flex-col items-center px-5">
+      <div className="relative z-50 flex -translate-y-60 flex-col items-center px-5">
         {children}
       </div>
+      <div className={styles.mouse}></div>
     </div>
   );
 };
-
-export function GridBackground() {
-  return (
-    <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
-      {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-        Backgrounds
-      </p>
-    </div>
-  );
-}
